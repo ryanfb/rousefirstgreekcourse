ca671a64606fab88eb6cfd38c00d78342860dc25
Ryan Baumann <ryan.baumann@gmail.com>
2020-04-13 13:07:08 -0400

    Add script

---
+++ scripts/generate-text.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -e "orig/rousegreekcourse-016.png" ]; then
+  echo "Splitting PDF into page images..."
+  pdfimages -png orig/RouseGreekCourseResized.pdf orig/rousegreekcourse
+fi
+while read i; do
+  TEXTFILE="text/$(basename $i .png | tr '-' '.')"
+  if [ ! -e "${TEXTFILE}.txt" ]; then
+    echo "Generating ${TEXTFILE}.txt from $i..."
+    tesseract -l eng+grc orig/$i "$TEXTFILE" txt
+    echo "Converting ${TEXTFILE}.txt to NFC..."
+    uconv -x any-nfc ${TEXTFILE}.txt > ${TEXTFILE}.nfc && mv -v ${TEXTFILE}.nfc ${TEXTFILE}.txt
+  fi
+done < scripts/reading_pages.txt
